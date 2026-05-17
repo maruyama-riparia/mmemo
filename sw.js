3ea91c2fcd161824c6e854f9b954c3b172f6778b
maruyama-riparia <maruyama@riparia.jp>
2026-05-17 18:53:29 +0900

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'm-memo-v10';
+const CACHE = 'm-memo-v11';
 const ASSETS = ['/', '/index.html', '/manifest.json', '/icons/icon-192.png', '/icons/icon-512.png'];
 
 self.addEventListener('install', e => {
