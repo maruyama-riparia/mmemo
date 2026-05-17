1e8d7e1ecb33a30544a45f3dd98bacc400c71602
maruyama-riparia <maruyama@riparia.jp>
2026-05-17 16:55:14 +0900

    Update sw.js

--- sw.js
+++ sw.js
@@ -3,14 +3,14 @@ const ASSETS = ['/', '/index.html', '/manifest.json', '/icons/icon-192.png', '/i
 
 self.addEventListener('install', e => {
   e.waitUntil(caches.open(CACHE).then(c => c.addAll(ASSETS)));
-  self.skipWaiting(); // 旧SWを即座に置き換え
+  self.skipWaiting();
 });
 
 self.addEventListener('activate', e => {
   e.waitUntil(caches.keys().then(keys =>
     Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k)))
-  ))
-  self.clients.claim(); // 既存タブをすぐ制御下に
+  ));
+  self.clients.claim();
 });
 
 self.addEventListener('fetch', e => {
@@ -19,7 +19,7 @@ self.addEventListener('fetch', e => {
   );
 });
 
-// 新バージョン検知したらクライアントに通知
+// クライアントからskipWaitingメッセージを受け取ったら即座に更新
 self.addEventListener('message', e => {
   if (e.data === 'skipWaiting') self.skipWaiting();
 });
