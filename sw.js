9a25f78745c62cd875595738c7d48e4513edfd47
maruyama-riparia <maruyama@riparia.jp>
2026-05-17 16:36:16 +0900

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'm-memo-v2';
+const CACHE = 'm-memo-v3';
 const ASSETS = ['/', '/index.html', '/manifest.json', '/icons/icon-192.png', '/icons/icon-512.png'];
 
 self.addEventListener('install', e => {
@@ -10,7 +10,7 @@ self.addEventListener('activate', e => {
   e.waitUntil(caches.keys().then(keys =>
     Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k)))
   ));
-  self.clients.claim();
+  self.clients.claim(); 
 });
 
 self.addEventListener('fetch', e => {
